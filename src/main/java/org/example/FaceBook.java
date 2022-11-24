7fce538b910feb107a3a9aacbea23202d77eef61
DB <bv.test.bm@gmail.com>
2022-11-24 22:11:26 +0000

    FaceBook

--- src/main/java/org/example/FaceBook.java
+++ src/main/java/org/example/FaceBook.java
@@ -2,14 +2,19 @@ package org.example;
 
 import org.openqa.selenium.By;
 
+import java.util.Set;
+
 public class FaceBook extends Utils
-{
-    private By _clickOnFaceBookLogo = By.linkText("http://www.facebook.com/nopCommerce");
-
-
-    public void faceBookButton()
     {
-        clickOnElement(_clickOnFaceBookLogo);
-    }
+        public void switchToChildWindowToParent()
+     {
+    // get the current window handle
+    String parentHandle = driver.getWindowHandle();
+ for(String handle: driver.getWindowHandles())
+  {
+    if (!parentHandle.equals(handle))
+        driver.switchTo().window(handle);
+  }
 
+      }
 }
